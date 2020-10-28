fa2fa244cacd53836d9f343e13914437f2bcc8b7
LizLaffitte <laffitte.digital@gmail.com>
2020-10-28 15:21:40 -0400

    Installed mailchimp module.

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,8 +1,27 @@
 var express = require('express')
 var router = express.Router()
+var xl = require('excel4node')
+const client = require("mailchimp-marketing");
+client.setConfig({
+    apiKey: "YOUR_API_KEY",
+    server: "YOUR_SERVER_PREFIX",
+  })
+
+client.setConfig({
+    apiKey: "YOUR_API_KEY",
+    server: "YOUR_SERVER_PREFIX",
+});
 
 router.get('/', function(req, res, next) {
-    res.send('API is working properly')
+    res.send('Campaigns')
 })
+
+router.get('/download', function(req, res) {
+    var wb = new xl.Workbook();
+    var ws = wb.addWorksheet('SHEET_NAME');
+    ws.cell(1, 1).string('ALL YOUR EXCEL SHEET FILE CONTENT');
+    wb.write(`FileName.xlsx`, res);
+})
+
 
 module.exports = router
