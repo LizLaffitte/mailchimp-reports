e40e10d7ab9966e7e53881f70bbaf05032c70c00
lizlaffitte <laffitte.digital@gmail.com>
2020-10-28 21:42:59 -0400

    Ensure headers error is resolved, that Excel file downloads on route visit.

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -8,50 +8,33 @@ client.setConfig({
     server: process.env.SERVER_PREFIX
   })
 
-router.use('/', function(req, res, next) {
-    client.reports.getAllCampaignReports().then(response => {
-        res.json(response)
-        next()    
-    }
-    )    
+const allCampaigns = async (req,res,next) => {
+  const data = await client.reports.getAllCampaignReports()
+  res.json(data)
+  next()
+}
+router.get('/', allCampaigns, (req,res) => {
+  res.end()
 })
 
-router.get('/', function(req, res) {
-  res.end()    
+const oneCampaign = async (req,res,next) => {
+  const data = await client.reports.getCampaignReport(req.params.campaignId)
+  res.json(data)
+  next()
+}
+
+router.get('/:campaignId', oneCampaign, (req,res) => {
+  res.end()
 })
 
 
-router.use('/:campaignId', function(req, res, next) {
-  console.log(req.params.campaignId)
-    client.reports.getCampaignReport(req.params.campaignId).then(response => {
-      
-        res.json(response)
-        next()    
-    }
-    ).catch(console.log("Error"))    
-})
-
-router.get('/:campaignId', function(req, res) {
-  res.end()    
-})
-
-router.get('/download', function(req, res) {
+router.get('/:campaignId/download', function(req, res) {
     var wb = new xl.Workbook();
     var ws = wb.addWorksheet('SHEET_NAME');
     ws.cell(1, 1).string('ALL YOUR EXCEL SHEET FILE CONTENT');
     wb.write(`FileName.xlsx`, res);
 })
 
-
 module.exports = router
 
-// const endpoint = 'https://jsonplaceholder.typicode.com/posts/1';
-// const asyncMiddleware = async (req,res,next) => {
-//   const data = await PromiseBasedDataRequest(endpoint);
-//   req.data = data.json()
-//   next()
-// }
-// app.get('/', asyncMiddleware, (req,res) => {
-//   const { title, body } = req.data;
-//   req.render('post', { title, body });
-// })
+
